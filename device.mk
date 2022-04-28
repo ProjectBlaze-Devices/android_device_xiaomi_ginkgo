f6625eab75ce8e5708b13ad3290ec3db52d5b77f
WisnuArdhi <wisnuardi128@gmail.com>
2022-04-28 05:00:18 +0000

    ginkgo: change MiuiCamera to GrapheneOSCamera

--- device.mk
+++ device.mk
@@ -84,9 +84,8 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml
 
 # Camera
-$(call inherit-product, vendor/miuicamera/config.mk)
-
 PRODUCT_PACKAGES += \
+    GrapheneOS-Camera \
     android.frameworks.displayservice@1.0.vendor \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
